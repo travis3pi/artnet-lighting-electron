cfa9b2cc33d0c2fbf667aabb447058dd341072d7
travis <travis3pi@gmail.com>
2020-08-12 11:40:05 -0700

    Performance improvements

--- sock.js
+++ sock.js
@@ -21,13 +21,38 @@ app.get('/', (req, res) => {
 
 server.on('message', (message) => {
     // console.log(message[18]+', '+ message[19]+', ' + message[20]);
+    lightArray = [];
     for (let i = 18; i < 500; i++) {
         if (!(i % 3)) {
             // console.log((i / 3) - 5)
-            socket.emit('light', {id: (i / 3) - 5, r: message[i], g: message[i + 1], b: message[i + 2]});
+            // socket.emit('light', {id: (i / 3) - 5, r: message[i], g: message[i + 1], b: message[i + 2]});
+            lightArray.push({id: (i / 3) - 5, r: message[i], g: message[i + 1], b: message[i + 2]});
         }
     }
-})
+    if (lightArray.length !== oldLightArray.length) {
+        oldLightArray = [...lightArray];
+        console.log('assigned')
+    }
+
+    //console.log(oldLightArray);
+    //console.log(lightArray);
+
+
+});
+
+setInterval(() => {
+    for (let i = 0; i < lightArray.length; i++) {
+        let light = lightArray[i];
+        let oldLight = oldLightArray.filter(obj => {
+            return obj.id === light.id
+        })[0];
+        if (light.r !== oldLight.r || light.g !== oldLight.g || light.b !== oldLight.b) {
+            socket.emit('light', light);
+        }
+
+    }
+    oldLightArray = [...lightArray];
+}, 45);
 
 
 socket.on('testMessage', (data) => {
