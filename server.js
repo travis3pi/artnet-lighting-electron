fde605a5a2638842ed89e0a4ad11989305e92b6d
travis <travis3pi@gmail.com>
2021-03-24 20:17:47 -0700

    Updated server.js to include agora code

--- server.js
+++ server.js
@@ -3,13 +3,48 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 
+var agoraToken = {
+    token: '',
+    channelName: ''
+};
+var agoraParams = {
+    zMin: 0,
+    zMax: 0,
+    nStr: 0
+}
+
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
 });
 
 
 io.on('connection', (socket) => {
+
     console.log('a user connected');
+    // socket.emit('agoraInfo', agoraToken);
+    // socket.emit('agoraParams', agoraParams);
+
+    socket.on('getAgoraInfo', function (data) {
+        socket.emit('agoraInfo', agoraToken)
+    })
+
+    socket.on('getAgoraParams', function (data) {
+        socket.emit('agoraParams', agoraParams)
+    })
+
+    socket.on('agoraInfo', function (data) {
+        console.log('agoraInfo', data)
+        agoraToken = data;
+        socket.broadcast.emit('agoraInfo', data);
+
+    });
+
+    socket.on('agoraParams', function (data) {
+        console.log('agoraParams', data)
+        agoraParams = data;
+        socket.broadcast.emit('agoraParams', data);
+    })
+
 
     socket.on('light', (data) => {
         // console.log(data);
@@ -26,6 +61,7 @@ io.on('connection', (socket) => {
 
     socket.on('agora', (data) => {
         io.emit('agora', data);
+        console.log('agora', data)
     })
 
     socket.on('scene', (data) => {
